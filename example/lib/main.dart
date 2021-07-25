c831ebe3daec5e14e502b8463a81d2d6d92e3538
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2021-07-25 09:38:17 +0200

    Added the cast demo from the cast package to start

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,59 +1,192 @@
 import 'package:flutter/material.dart';
-
-import 'package:flutter/services.dart';
-import 'package:cast_ui/cast_ui.dart';
+import 'package:cast/cast.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  _MyAppState createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cast UI Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          brightness: Brightness.dark,
+          title: const Text(
+            'Cast UI Demo',
+          ),
+        ),
+        body: MyHomePage(),
+      ),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future<List<CastDevice>>? _future;
+  CastSession? session;
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await CastUi.platformVersion ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
+    _startSearch();
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
-        ),
-      ),
+    return FutureBuilder<List<CastDevice>>(
+      future: _future,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Center(
+            child: Text(
+              'Error: ${snapshot.error.toString()}',
+            ),
+          );
+        } else if (!snapshot.hasData) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        if (snapshot.data!.isEmpty) {
+          return const Center(
+            child: Text(
+              'No Chromecast founded',
+            ),
+          );
+        }
+
+        final realData = snapshot.data?.where((element) => true).toList() ?? [];
+        return Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: realData.length,
+                itemBuilder: (context, index) {
+                  final device = realData[index];
+                  return ListTile(
+                    title: Text(device.name),
+                    onTap: () {
+                      // _connectToYourApp(context, device);
+                      _connectAndPlayMedia(context, device);
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        );
+      },
     );
+  }
+
+  void _startSearch() {
+    _future = CastDiscoveryService().search();
+  }
+
+  // ignore: unused_element
+  Future<void> _connectToYourApp(BuildContext context, CastDevice object) async {
+    final session = await CastSessionManager().startSession(object);
+    setState(() {
+      this.session = session;
+    });
+    session.stateStream.listen((state) {
+      if (state == CastSessionState.connected) {
+        const snackBar = SnackBar(content: Text('Connected'));
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+        _sendMessageToYourApp(session);
+      }
+    });
+
+    session.messageStream.listen((message) {
+      print('receive message: $message');
+    });
+
+    session.sendMessage(CastSession.kNamespaceReceiver, {
+      'type': 'LAUNCH',
+      'appId': 'B6DF242C', // set the appId of your app here
+    });
+  }
+
+  void _sendMessageToYourApp(CastSession session) {
+    print('_sendMessageToYourApp');
+
+    session.sendMessage('urn:x-cast:namespace-of-the-app', {
+      'type': 'sample',
+    });
+  }
+
+  Future<void> _connectAndPlayMedia(BuildContext context, CastDevice object) async {
+    final session = await CastSessionManager().startSession(object);
+    setState(() {
+      this.session = session;
+    });
+    session.stateStream.listen((state) {
+      if (state == CastSessionState.connected) {
+        const snackBar = SnackBar(content: Text('Connected'));
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      }
+    });
+
+    var index = 0;
+
+    session.messageStream.listen((message) {
+      index += 1;
+
+      print('receive message: $message');
+
+      if (index == 2) {
+        Future.delayed(const Duration(seconds: 5)).then((x) {
+          _sendMessagePlayVideo(session);
+        });
+      }
+    });
+
+    session.sendMessage(CastSession.kNamespaceReceiver, {
+      'type': 'LAUNCH',
+      'appId': 'B6DF242C', // set the appId of your app here
+    });
+  }
+
+  void _sendMessagePlayVideo(CastSession session) {
+    print('_sendMessagePlayVideo');
+
+    final message = {
+      // Here you can plug an URL to any mp4, webm, mp3 or jpg file with the proper contentType.
+      'contentId': 'http://commondatastorage.googleapis.com/gtv-videos-bucket/big_buck_bunny_1080p.mp4',
+      'contentType': 'video/mp4',
+      'streamType': 'BUFFERED', // or LIVE
+
+      // Title and cover displayed while buffering
+      'metadata': {
+        'type': 0,
+        'metadataType': 0,
+        'title': 'Big Buck Bunny',
+        'images': [
+          {
+            'url': 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg',
+          }
+        ]
+      }
+    };
+
+    session.sendMessage(CastSession.kNamespaceMedia, {
+      'type': 'LOAD',
+      'autoPlay': true,
+      'currentTime': 0,
+      'media': message,
+    });
   }
 }
