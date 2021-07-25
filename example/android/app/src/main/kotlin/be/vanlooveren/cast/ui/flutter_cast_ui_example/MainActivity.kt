05e506989cd10a7fbe46984e1e50ea36102142cd
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2021-07-25 08:39:15 +0200

    Initial commit

---
+++ example/android/app/src/main/kotlin/be/vanlooveren/cast/ui/flutter_cast_ui_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package be.vanlooveren.cast.ui.flutter_cast_ui_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ios/Classes/FlutterCastUiPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface FlutterCastUiPlugin : NSObject<FlutterPlugin>
+@end
